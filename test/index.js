5999f894bd06d518755183335074e2420fd4e153
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-18 12:52:43 +0900

    Create package.json.

--- test/index.js
+++ test/index.js
@@ -4,16 +4,27 @@ var assert = require('yeoman-generator').assert;
 
 describe('gblo generator', function () {
 
+  var expected = [
+    'package.json'
+  ];
+
+  var expectedContent = [
+    ['package.json', /"name": "tmp"/]
+  ];
+
   var runGen;
 
-  beforeEach(function(done) {
+  beforeEach(function() {
     runGen = helpers
       .run(path.join(__dirname, '../app'))
-      .inDir(path.join(__dirname, '.tmp'))
-      .on('end', done);
+      .inDir(path.join(__dirname, '.tmp'));
   });
 
-  it('should has name property with gblo', function () {
-    assert.deepEqual(runGen.generator.config.name, 'generator-gblo');
+  it('create expexted files', function (done) {
+    runGen.on('end', function () {
+      assert.file(expected);
+      assert.fileContent(expectedContent);
+      done();
+    });
   });
 });
